d8adc0581e5848add87f76ececca44da7e761b1b
GrubeB <dawidbladek0831@gmail.com>
2024-08-02 03:43:27 +0200

    voting-service - dto module

---
+++ service/voting/voting-dto/src/main/java/pl/app/comment/query/dto/VotingDto.java
@@ -0,0 +1,27 @@
+package pl.app.comment.query.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VotingDto implements Serializable {
+    private ObjectId id;
+    private String domainObjectType;
+    private String domainObjectId;
+    private Set<VoteCounterDto> votes;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class VoteCounterDto implements Serializable {
+        private String type;
+        private Long number;
+    }
+}
